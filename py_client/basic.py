611b68a60796b8164a8cc80972f49a313f754a28
unknown <abdoulaye.deng@gmail.com>
2023-05-13 23:21:34 +0000

    Section 5 start - end

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,13 +1,10 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000"
 
 get_response = requests.get(endpoint, json={"query": "Mr Robot"})
 
 print("Response as raw text: ")
 print(get_response.text)
-
-print("Response as JSON")
-print(get_response.json())
-
 print(f"Response Status code: {get_response.status_code}")
