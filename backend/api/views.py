10b4cb0ccc6ddf6fd757483cba6968260b1ccd7e
unknown <abdoulaye.deng@gmail.com>
2023-05-14 16:38:13 +0000

    Section 9 start

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 import json
 from products.models import Product
@@ -6,11 +7,10 @@ from products.models import Product
 def api_home(request, *args, **kwargs):
     # Get the request body (data sent by the client)
     # The request param here is an instange of django HttpRequest
-    # and has nothing to do with "pythin requests"
+    # and has nothing to do with "python requests"
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
+        data = model_to_dict(model_data)
+
     return JsonResponse(data)
