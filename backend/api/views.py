71817bde8c7b4649795a3fb1642fe4a5a883fceb
unknown <abdoulaye.deng@gmail.com>
2023-05-14 16:52:22 +0000

    Section 9 end: Rest Framework View and Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,12 @@
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 import json
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from products.models import Product
 
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
     # Get the request body (data sent by the client)
     # The request param here is an instange of django HttpRequest
@@ -13,4 +16,4 @@ def api_home(request, *args, **kwargs):
     if model_data:
         data = model_to_dict(model_data)
 
-    return JsonResponse(data)
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,10 +3,11 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-params = {"product_id": 25}
-get_response = requests.get(endpoint, params=params, json={"query": "Mr Robot"})
+get_response = requests.get(endpoint, json={"query": "Mr Robot"})
 
-print("Response as raw text: ")
+# print("Response as raw text: ")
 # print(get_response.text)
+
+print("Response JSON: ")
 print(get_response.json())
 print(f"Response Status code: {get_response.status_code}")
